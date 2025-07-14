f648f79f2fd04c5f08c32d046350b43e43d7dab8
ismail.b.youssef@gmail.com <ismail.b.youssef@gmail.com>
2025-07-14 22:22:39 +0200

    Fix probleme Cors

---
+++ back/src/main/java/com/alten/back/configuration/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.alten.back.configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(Arrays.asList("http://localhost:4200")); // 🔁 Utilise `AllowedOriginPatterns` au lieu de `AllowedOrigins`
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 🔁 toutes les routes
+
+        return new CorsFilter(source);
+    }
+}
